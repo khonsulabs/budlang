9d9621bc374f07bcf5214859a7bcd73f8ba410c0
kl-botsu <botsu@khonsulabs.com>
2022-11-20 03:48:50 +0000

    Deploying to gh-pages from @ khonsulabs/budlang@b023827957cc04fbff2f13b9b48d66eede3d2980 🚀

---
+++ main/budvm/ir/asm/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["AsmError",""],["TokenKind",""]],"struct":[["Lexer",""],["Token",""]]};
--- main/source-files.js
+++ main/source-files.js
@@ -1,7 +1,7 @@
 var sourcesIndex = JSON.parse('{\
 "bud":["",[],["bud.rs"]],\
 "budlang":["",[],["ast.rs","lib.rs","parser.rs"]],\
-"budvm":["",[],["budmap.rs","dynamic.rs","ir.rs","lib.rs","list.rs","map.rs","string.rs","symbol.rs"]],\
+"budvm":["",[["ir",[],["asm.rs"]]],["budmap.rs","dynamic.rs","ir.rs","lexer_util.rs","lib.rs","list.rs","map.rs","string.rs","symbol.rs"]],\
 "xtask":["",[],["main.rs"]]\
 }');
 createSourceSidebar();
